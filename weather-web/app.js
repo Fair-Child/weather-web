64086892831073e6e08f8a97e31284e7d413df1f
Brent <brentliang>
2020-07-23 21:01:57 -0400

    update of request

--- weather-web/app.js
+++ weather-web/app.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=1dd6355046295a9a2b056b726f08ac0d&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=1dd6355046295a9a2b056b726f08ac0d&query=37.8267,-122.4233&units=m'
 
-request({url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ' ' + response.body.current.temperature + ' degrees.')
 })
