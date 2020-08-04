4183789d0cb768f291e1aaa825aaa54940f9ddfa
Brent <brentliang>
2020-08-04 16:30:00 -0400

    geocode dependency added

--- weather-web/app.js
+++ weather-web/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'http://api.weatherstack.com/current?access_key=1dd6355046295a9a2b056b726f08ac0d&query=37.8267,-122.4233&units=m'
 
@@ -15,17 +16,10 @@ const request = require('request')
 
 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/toronto.json?access_token=pk.eyJ1IjoiYnJlbnRsaWFuZyIsImEiOiJja2QzaWt2enMwNWNqMnhvZDQzNGJjbHpyIn0.QRdAa4F7GGPFm_26QIQ6sA&limit=1'
 
-request({url: geocodeURL, json: true}, (error, response) => {
-    if (error) {
-        console.log("Unable to connect to the location services.")
-    } else if (response.body.message === "Not Found") {
-        console.log("Unable to find location. Try another search.")
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
-    
+geocode('Toronto', (error, data) => {
+
+    console.log('Error', error)
+    console.log('Data', data)
+
 })
